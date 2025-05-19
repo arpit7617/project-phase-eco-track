0fe7e20474e9d0f0bf4565c0af1c7ef10a835499
Aayush Kukreja <aayushkukreja0104@gmail.com>
2025-05-20 01:55:00 +0530

    Fixed map to show fixed coordinates on Location page

--- routes/community.js
+++ routes/community.js
@@ -5,7 +5,7 @@ const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database(path.join(__dirname, "..", "database.db"));
 
-// Middleware to protect community route
+// Middleware to protect community route 
 function isLoggedIn(req, res, next) {
   if (req.session.user) return next();
   res.redirect("/login");
